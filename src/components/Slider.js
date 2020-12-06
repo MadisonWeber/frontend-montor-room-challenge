31b5587c8a2670e25d852ad31787618303adf679
Madison Weber <madison.weber.dev@gmail.com>
2020-12-05 19:22:28 -0500

    slider accessability

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -10,8 +10,8 @@ const Slider = () => {
 
     return (
         <div className = {currentlyMobile ? 'slider__mobile' : 'slider'}>
-            <button className = 'slider__left' onClick = {decreaseSlideNum}><svg width="14" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M13 0L1 12l12 12" stroke="#FFF" fill="none" fillRule="evenodd"/></svg></button>
-            <button className = 'slider__right' onClick = {increaseSlideNum}><svg width="14" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M1 0l12 12L1 24" stroke="#FFF" fill="none" fillRule="evenodd"/></svg></button>
+            <button className = 'slider__left' aria-label = {'change slide'} onClick = {decreaseSlideNum} ><svg width="14" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M13 0L1 12l12 12" stroke="#FFF" fill="none" fillRule="evenodd"/></svg></button>
+            <button className = 'slider__right' onClick = {increaseSlideNum} aria-label = {'change slide'}><svg width="14" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M1 0l12 12L1 24" stroke="#FFF" fill="none" fillRule="evenodd"/></svg></button>
         </div>
     )
 }
